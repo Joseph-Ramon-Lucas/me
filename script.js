e569498bbfe3a8a55b922e47dbd6bf0af0256083
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2023-12-29 19:45:45 -0500

    setting up script

---
+++ script.js
@@ -0,0 +1,7 @@
+let socials = document.getElementById("socialsButton");
+
+let socialSelect = false;
+socials.addEventListener("click", () => {
+    socialSelect = true;
+    console.log(socialSelect);
+})
